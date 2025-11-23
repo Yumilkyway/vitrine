49a703769a02c5c966380daa94f1dc4a2842ba4b
Fernanda Yumi <160788898+Yumilkyway@users.noreply.github.com>
2025-11-23 14:34:41 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ProductDetail from './pages/ProductDetail';
-import './App.css';
-import './styles/custom.css'
 
 function App() {
   return (
     <Router>
       <Routes>
-        {}
         <Route path="/" element={<Home />} /> 
-        
-        {}
         <Route path="/produto/:productId" element={<ProductDetail />} /> 
-        
-        {}
         <Route path="*" element={
           <div className="text-center my-5">
             <h2>Página Não Encontrada 💔</h2>
