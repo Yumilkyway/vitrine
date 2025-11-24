ffcbbd0147a2b7e8651fc728f0cd3a0be521f22d
Fernanda Yumi <160788898+Yumilkyway@users.noreply.github.com>
2025-11-24 09:29:28 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './styles/custom.css'
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
